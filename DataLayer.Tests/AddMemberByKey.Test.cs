97963eb09d8d5029d637d7136d52517da316e27a
Zac Davis <thatzacdavis@gmail.com>
2021-08-05 03:19:16 -0400

    Unit test RemoveMemberByKey

--- DataLayer.Tests/AddMemberByKey.Test.cs
+++ DataLayer.Tests/AddMemberByKey.Test.cs
@@ -5,11 +5,14 @@ using System.Linq;
 
 namespace DataLayer.Tests
 {
+    [Collection("Serial")]
     public class AddMemberByKey
     {
         [Fact]
         public void AddsNewKeyWhenMemberAddedForNewKey()
         {
+            console_dictionary_app.DataLayer.ClearDictionary();
+
             console_dictionary_app.DataLayer.AddMemberByKey("zac", "davis");
             Assert.Single(console_dictionary_app.DataLayer._dictionary);
         }
@@ -17,6 +20,8 @@ namespace DataLayer.Tests
         [Fact]
         public void AddsSuppliedKeyAndValue()
         {
+            console_dictionary_app.DataLayer.ClearDictionary();
+
             console_dictionary_app.DataLayer.AddMemberByKey("zac", "davis");
             Assert.Equal("zac", console_dictionary_app.DataLayer._dictionary.First().Key);
             Assert.Equal("davis", console_dictionary_app.DataLayer._dictionary.First().Value.First());
@@ -25,6 +30,8 @@ namespace DataLayer.Tests
         [Fact]
         public void AddsSuppliedValueToExistingKey()
         {
+            console_dictionary_app.DataLayer.ClearDictionary();
+
             console_dictionary_app.DataLayer.AddMemberByKey("zac", "davis");
             console_dictionary_app.DataLayer.AddMemberByKey("zac", "davis2");
             Assert.Equal("davis2", console_dictionary_app.DataLayer._dictionary.First().Value.ElementAt(1));
