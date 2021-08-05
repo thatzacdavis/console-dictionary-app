cf7b6862b701cb9d11a207bd3d0d3e83d55039d1
Zac Davis <thatzacdavis@gmail.com>
2021-08-05 00:17:47 -0400

    Initial app creation
    Creates the app with the add, keys, and members commands.
    
    Also makes it easy to exit the app as well.

---
+++ DataLayer.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace console_dictionary_app
+{
+    public class DataLayer
+    {
+        public static Dictionary<string, List<string>> _dictionary = new Dictionary<string, List<string>>();
+
+        // Adds a member to the list of strings for an existing key,
+        // or creates a new entry in the dictionary if the key does not exist.
+        public static void Add(string key, string value)
+        {
+            if (!_dictionary.ContainsKey(key))
+            {
+                _dictionary.Add(key, new List<string>() { value });
+            }
+            else
+            {
+                KeyValuePair<string, List<string>> keyValuePair = _dictionary.First(item => item.Key == key);
+                keyValuePair.Value.Add(value);
+            }
+
+        }
+
+        public static void Keys()
+        {
+            Dictionary<string, List<string>>.KeyCollection keys = _dictionary.Keys;
+            foreach (var key in keys)
+            {
+                Console.WriteLine(key);
+            }
+        }
+
+        public static void Members(string key)
+        {
+            var items = _dictionary.Where(item => item.Key == key);
+            foreach (var item in items)
+            {
+                foreach(var subItem in item.Value) 
+                {
+                    Console.WriteLine(subItem);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace console_dictionary_app
+{
+    class Program
+    {
+        static void NotifyValidAction(string action) 
+        {
+            Console.WriteLine("Valid Action Entered: " + action);
+        }
+
+        static void NotifyNewAction(string action) 
+        {
+            Console.WriteLine("Enter a new action to proceed or EXIT to end your session..");
+        }
+
+        static void AcceptInput()
+        {
+            Console.WriteLine("Enter a valid action to proceed.");
+
+            string input = Console.ReadLine();
+            string[] inputs = input.Split(' ');
+
+            switch (inputs[0])
+            {
+                case "ADD":
+                    NotifyValidAction(inputs[0]);
+                    DataLayer.Add(inputs[1], inputs[2]);
+                    AcceptInput();
+                    break;
+                case "KEYS":
+                    NotifyValidAction(inputs[0]);
+                    DataLayer.Keys();
+                    AcceptInput();
+                    break;
+                case "MEMBERS":
+                    NotifyValidAction(inputs[0]);
+                    DataLayer.Members(inputs[1]);
+                    AcceptInput();
+                    break;
+                case "EXIT":
+                    NotifyValidAction(inputs[0]);
+                    Console.WriteLine("Exiting application...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Action Entered");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            AcceptInput();
+
+            // Console.Write($"{Environment.NewLine}Press any key to exit...");
+            // Console.ReadKey(true);
+
+            // var name = Console.ReadLine();
+            // var currentDate = DateTime.Now;
+            // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            // Console.Write($"{Environment.NewLine}Press any key to exit...");
+            // Console.ReadKey(true);
+        }
+    }
+}
